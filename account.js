9d7cdfc7651d9bc5d8066835316ed16cb586607d
Sanjeev Kumar <129384708+snjkumar23@users.noreply.github.com>
2023-11-06 11:34:30 +0530

    Create account.js

---
+++ account.js
@@ -0,0 +1,13 @@
+export const getAccountById = (id) => {
+    return id + 10000
+}
+
+
+export const getAccountDetailsByName = (name) => {
+    return "Account Details for names are : Dummy"
+}
+
+
+export const getAllAccounts = () => {
+    return [121334, 3223344, 23423423]
+}
